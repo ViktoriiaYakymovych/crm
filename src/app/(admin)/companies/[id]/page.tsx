8612fd518b291b02da8bc80a69436e427e62f74c
ViktoriiaYakymovych <118008244+ViktoriiaYakymovych@users.noreply.github.com>
2024-04-17 19:27:45 +0300

    intercepting routes

--- src/app/(admin)/companies/[id]/page.tsx
+++ src/app/(admin)/companies/[id]/page.tsx
@@ -1,13 +1,11 @@
-import Header from '@/app/components/header';
-
 export interface PageProps {
   params: { id: string };
 }
 
 export default function Page({ params }: PageProps) {
   return (
-    <>
-      <Header>Company ({params.id})</Header>
-    </>
+    <div className="py-6 px-10">
+      <p>{`Information about company (${params.id})`}</p>
+    </div>
   );
 }
--- src/app/(admin)/companies/page.tsx
+++ src/app/(admin)/companies/page.tsx
@@ -1,31 +1,21 @@
-import AddCompanyButton from '../../components/add-company-button';
 import CompanyRow from '../../components/company-row';
 import CompanyTable from '../../components/company-table';
-import Header from '../../components/header';
-import SearchInput from '../../components/search-input';
 import { Status } from '../../components/status-label';
-import Toolbar from '../../components/toolbar';
 
 export interface PageProps {}
 
 export default function Page({}: PageProps) {
   return (
-    <>
-      <Header>Companies</Header>
-      <Toolbar action={<AddCompanyButton />}>
-        <SearchInput />
-      </Toolbar>
-      <CompanyTable>
-        <CompanyRow
-          id={1}
-          category={'Products'}
-          company={'Costco'}
-          status={Status.Pending}
-          promotion={true}
-          country={'USA'}
-          joinedDate="01.01.2020"
-        />
-      </CompanyTable>
-    </>
+    <CompanyTable>
+      <CompanyRow
+        id={1}
+        category={'Products'}
+        company={'Costco'}
+        status={Status.Pending}
+        promotion={true}
+        country={'USA'}
+        joinedDate="01.01.2020"
+      />
+    </CompanyTable>
   );
 }
